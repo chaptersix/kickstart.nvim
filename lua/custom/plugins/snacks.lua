fc0910041d20c2b351c9a84577a00943bdce1518
chaptersix <chaptersix@users.noreply.github.com>
2025-12-14 15:39:56 -0600

    update

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -44,6 +44,9 @@ return {
         height = 0.8,
         backdrop = 60,
       },
+      mappings = {
+        t = { ['<Esc>'] = [[<C-\><C-n>]] },
+      },
     },
     words = { enabled = true },
     styles = {
@@ -161,31 +164,4 @@ return {
       desc = 'Explorer',
     },
   },
-  init = function()
-    Snacks = require 'snacks'
-    vim.api.nvim_create_autocmd('User', {
-      pattern = 'VeryLazy',
-      callback = function()
-        -- Setup some globals for debugging (lazy-loaded)
-        _G.dd = function(...)
-          Snacks.debug.inspect(...)
-        end
-        _G.bt = function()
-          Snacks.debug.backtrace()
-        end
-        vim.print = _G.dd -- Override print to use snacks for `:=` command
-
-        -- Create some toggle mappings
-        Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>us'
-        Snacks.toggle.option('wrap', { name = 'Wrap' }):map '<leader>uw'
-        Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map '<leader>uL'
-        Snacks.toggle.diagnostics():map '<leader>ud'
-        Snacks.toggle.line_number():map '<leader>ul'
-        Snacks.toggle.option('conceallevel', { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map '<leader>uc'
-        Snacks.toggle.treesitter():map '<leader>uT'
-        Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>ub'
-        Snacks.toggle.inlay_hints():map '<leader>uh'
-      end,
-    })
-  end,
 }
